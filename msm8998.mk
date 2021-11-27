d3b17a8689bf42b4a8831f734edddca1a72a74bb
Jrchintu <Harrypetersun@outlook.com>
2021-11-27 11:56:50 +0100

    msm8998-common: Build android.hardware.drm@1.2.vendor
    Signed-off-by: Jrchintu <Harrypetersun@outlook.com>
    
    Change-Id: Ic361c4c2af255c2c75180741a105ff37b3173d53

--- msm8998.mk
+++ msm8998.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey
 
 # Fingerprint
