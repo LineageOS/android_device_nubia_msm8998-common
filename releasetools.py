af104a2c8c972f44edad183b254d7bf32db43083
Alessandro Astone <ales.astone@gmail.com>
2022-09-09 08:21:51 +0000

    msm8998-common: releasetools: Make re.search work on python3
    TypeError: cannot use a string pattern on a bytes-like object
    
    Change-Id: I9e811f3d170402d23fd0448f1a21496a8c56bf88

--- releasetools.py
+++ releasetools.py
@@ -38,7 +38,7 @@ abort("Error: Vendor partition doesn\'t exist!"););'
   return
 
 def AddModemAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+  android_info = info.input_zip.read("OTA/android-info.txt").decode('UTF-8')
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   f = re.search(r'require\s+version-firmware\s*=\s*(.+)', android_info)
   if m and f:
