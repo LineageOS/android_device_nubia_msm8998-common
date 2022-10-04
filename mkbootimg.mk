758270c2c310fe8bd21d6b51a9f538efce529e70
Aaron Kling <webgeek1234@gmail.com>
2022-10-04 17:56:58 -0500

    msm8998-common: mkbootimg: Boot image depends on kernel
    Very rarely, the boot image tries to build before the kernel is done
    
    Change-Id: I14c85c0ed681c4ff2eaf268752c4e2ff94f9777f

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -7,7 +7,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ## Boot Image Generation
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
