kracenra/android_device_nubia_msm8998-common-oss

msm8998-common: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
extends system sepolicy. To that end, we use another make variable to
specify product sepolicy.

Bug: 119305624
Test: m selinux_policy
Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f
(cherry picked from commit 278f95b1bf97c74375f420d2ac15de1a2cc7628f)
(cherry picked from commit b25fccdc43e4a3e22aa5a95fadbafab00362acf8)

BoardConfigCommon.mk
@@ -151,8 +151,8 @@ TARGET_USES_OLD_MNC_FORMAT := true
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true

LineageOS/android_device_nubia_msm8998-common

BoardConfigCommon.mk
@@ -144,7 +144,7 @@ VENDOR_SECURITY_PATCH := 2020-11-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 
 # Treble