f36563ed136a3ca73988c1853ce64b5ccbdefc31
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    msm8998-common: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- msm8998.mk
+++ msm8998.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service.legacy
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
