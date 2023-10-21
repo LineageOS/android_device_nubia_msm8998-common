33fc2aedca482d610b39a5b3bacd4f1100368c9e
LuK1337 <priv.luk@gmail.com>
2023-10-21 15:44:06 +0200

    msm8998-common: Build android.hardware.bluetooth@1.0
    Required by libantradio.
    
    Change-Id: I5e26a4154cf54e64f424cb6cf20db87b7023f1cd

--- msm8998.mk
+++ msm8998.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth audio
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
