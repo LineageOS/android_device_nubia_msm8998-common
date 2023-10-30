1d6285907f49b770b301a7193b2b4bce69719340
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 17:38:30 +0200

    msm8998-common: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca91c

--- msm8998.mk
+++ msm8998.mk
@@ -9,6 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
