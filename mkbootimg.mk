f87e09a2134c5e4d2754bc5818cb7c960ea846e7
Cyborg2017 <wangyang0987654@gmail.com>
2023-11-12 15:15:29 +0800

    msm8998-common: Add prebuilt AVB 1.0 signing support to our devices
    * Google has already removed AVB 1.0 on A14, but our bootloader doesn’t accept unsigned boot/recovery images.
    
      Import verity keys, sign boot/recovery images using custom mkbootimg.mk.
    
    * verity keys from A13: https://android.googlesource.com/platform/build/+/refs/tags/android-13.0.0_r83/target/product/security/
    
    Change-Id: I6bcfffd46f642a4d5732d37943a3e405d62df945

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -6,10 +6,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
+TOOLS_PATH := $(PLATFORM_PATH)/tools
+
 ## Boot Image Generation
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(TOOLS_PATH)/boot_signer /boot $@ $(TOOLS_PATH)/verity.pk8 $(TOOLS_PATH)/verity.x509.pem $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo "Made boot image: $@"
 
@@ -20,5 +23,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
 		$(call build-recoveryimage-target, $@)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(TOOLS_PATH)/boot_signer /recovery $@ $(TOOLS_PATH)/verity.pk8 $(TOOLS_PATH)/verity.x509.pem $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo "Made recovery image: $@"
