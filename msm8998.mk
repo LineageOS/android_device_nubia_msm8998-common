1370d6662df7b25f0dbf6aba20701cc68fc5014d
Cosmin Tanislav <demonsingur@gmail.com>
2024-07-29 23:34:33 +0300

    msm8998-common: build libstagefright_softomx_plugin
    vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found
    
    Change-Id: I56b620ebb9f9dc64ecc492bdb7ee17c3df57e593

--- msm8998.mk
+++ msm8998.mk
@@ -240,7 +240,8 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # Partitions
 PRODUCT_PACKAGES += \
