4fe4c5b2e7a556832eeeb5f3e788e53bad90adc3
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    msm8998-common: Shim libwvhidl with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- msm8998.mk
+++ msm8998.mk
@@ -170,7 +170,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim
 
 # Fastbootd
 PRODUCT_PACKAGES += \
