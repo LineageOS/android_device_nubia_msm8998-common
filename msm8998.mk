01306daea1a61fc2a19b119d131bfb00f08f27f8
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:57:40 -0400

    msm8998-common: Ensure we build the vendor version of libcrypto_shim
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: I9c9340b37a1ad0d6c22d3947541fe1196a5efeec

--- msm8998.mk
+++ msm8998.mk
@@ -171,7 +171,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim
+    libcrypto_shim.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
