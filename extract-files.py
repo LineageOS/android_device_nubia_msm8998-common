b0e5c0ee55663a295cd7f70d5ce4dd13f5681019
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-03 17:09:20 -0500

    msm8998-common: Patch widevine to replace libhidltransport with libhidlbase and drop libhwbinder
    Change-Id: I1847fe100e9d5dafd8ddfcb9456c7559b64d7049

--- extract-files.py
+++ extract-files.py
@@ -59,6 +59,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.radio.config@1.0.so', 'android.hardware.radio.c_shim@1.0.so')
         .replace_needed('android.hardware.radio.config@1.1.so', 'android.hardware.radio.c_shim@1.1.so')
         .replace_needed('android.hardware.radio.config@1.2.so', 'android.hardware.radio.c_shim@1.2.so'),
+    'vendor/bin/hw/android.hardware.drm@1.1-service.widevine': blob_fixup()
+        .replace_needed('libhidltransport.so', 'libhidlbase.so')
+        .remove_needed('libhwbinder.so'),
     'vendor/lib64/libwvhidl.so': blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/vendor.nubia.ifaa@1.0.so': blob_fixup()
