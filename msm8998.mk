9e14f8daa88da8b6960e6d860da05d26f7f33ee3
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    msm8998-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- msm8998.mk
+++ msm8998.mk
@@ -146,7 +146,7 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
-    hwcomposer.msm8998 \
+    hwcomposer.qcom \
     libdisplayconfig \
     libqdMetaData.system
 
