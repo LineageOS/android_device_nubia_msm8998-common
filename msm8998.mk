9682793cbe763576ef960fcad5ff5c4bb0b99ac0
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    msm8998-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- msm8998.mk
+++ msm8998.mk
@@ -106,10 +106,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong
-
 # Bluetooth audio
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
